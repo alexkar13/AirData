7b668412093c02109e3f77f75062dce25b756910
Alex Karydis <alexkarydis@mail.com>
2017-10-12 15:49:06 +0200

    added src folder

---
+++ src/ConfigValues.java
@@ -0,0 +1,62 @@
+// package src;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class ConfigValues {
+    private String dbName;
+    private String host;
+    private String user;
+    private String pass;
+    private String port;
+
+    InputStream inputStream;
+    Properties prop = new Properties();
+    // String propFileName = "/../conf/config.properties";
+
+    public void loadInput(String path) throws IOException{
+        try{
+            inputStream = getClass().getClassLoader().getResourceAsStream(path);
+
+            if (inputStream != null) {
+                prop.load(inputStream);
+                System.out.println("Input Stream is loaded.");
+            } else {
+                throw new FileNotFoundException("property file '" + path + "' not found in the classpath");
+            }
+
+            user = prop.getProperty("user");
+            port = prop.getProperty("port");
+            pass = prop.getProperty("pass");
+            host = prop.getProperty("host");
+            dbName = prop.getProperty("dbName");
+        } catch (Exception e){
+            System.out.println("Exception" + e);
+        } finally {
+            inputStream.close();
+        }
+    }
+
+    public String getUser(){
+        return user;
+    }
+
+    public String getPort(){
+        return port;
+    }
+
+    public String getPass(){
+        return pass;
+    }
+
+    public String getHost(){
+        return host;
+    }
+
+    public String getDbName(){
+        return dbName;
+    }
+
+}
---
+++ src/SelectQueries.java
@@ -0,0 +1,38 @@
+// package src;
+// All the mySQL select queries needed for the application are stored here
+// Using getters so that fields remain private
+public class SelectQueries{
+
+  // Queries will have to be appended later on
+  public String countryQuery =  "SELECT airports.name, runways.le_ident, runways.he_ident FROM countries, airports, runways WHERE runways.airport_ref = airports.id and airports.iso_country = countries.code and countries.name = '";
+  public String countryCodeQuery = "SELECT airports.name, runways.le_ident, runways.he_ident FROM airports, runways WHERE airports.ident = runways.airport_ident and airports.iso_country = '";
+  // Complete Queries
+  private String mostAiportsReports =  "SELECT countries.name, count(*) FROM airports, countries WHERE countries.code = airports.iso_country GROUP by countries.name ORDER by count(*) desc limit 10;";
+  private String leastAiportsReports =  "SELECT countries.name, count(*) FROM airports, countries WHERE countries.code = airports.iso_country GROUP by countries.name ORDER by count(*) asc limit 10;";
+  private String surfaceReports = "select distinct countries.name as countries, runways.surface as surface from airports,runways,countries where countries.code = airports.iso_country and airports.id = runways.airport_ref order by countries.name;";
+  private String leIdentTopTenReports = "select le_ident as le_ident, count(*) as number from runways group by le_ident order by count(*) desc limit 10;";
+
+  public String getCountryQuery(String httpReturnParameter) {
+      return countryQuery + httpReturnParameter + "';";
+  }
+
+  public String getCountryCodeQuery(String httpReturnParameter) {
+      return countryCodeQuery + httpReturnParameter + "';";
+  }
+
+  public String getMostAirportsReports() {
+      return mostAiportsReports;
+  }
+
+  public String getLeastAirportsReports() {
+      return leastAiportsReports;
+  }
+
+  public String getSurfaceReports() {
+      return surfaceReports;
+  }
+
+  public String getLeIdentTopTenReports() {
+      return leIdentTopTenReports;
+  }
+}
